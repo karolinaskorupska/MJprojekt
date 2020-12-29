bd31e5a03bd645723f2cccfcc006bc041680ea48
karolinaskorupska <pixie22@op.pl>
2020-12-29 18:48:55 +0100

    index css poprawiony

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@
 const hamburgerMenu = document.getElementById("hamburgerMenu");
 const container = document.getElementById("container");
 const backgroundImage = document.getElementById("logoImage");
+const footerText = document.getElementById("footer");
 
 hamburgerMenu.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("active");
     container.classList.toggle("active");
     backgroundImage.classList.toggle("active");
+    footerText.classList.toggle("active");
 
 });
 
